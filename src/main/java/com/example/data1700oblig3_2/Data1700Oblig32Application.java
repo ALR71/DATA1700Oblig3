adb35ee731cd174354ba7f7a6e4cfff5351f3933
ALR71 <alalr7105@oslomet.no>
2024-04-24 21:11:38 +0200

    starting project

---
+++ src/main/java/com/example/data1700oblig3_2/Data1700Oblig32Application.java
@@ -0,0 +1,13 @@
+package com.example.data1700oblig3_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700Oblig32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Data1700Oblig32Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/data1700oblig3_2/Data1700Oblig32ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data1700oblig3_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700Oblig32ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
